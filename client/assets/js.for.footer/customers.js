b7505519e3b66b6f54cf642106743b793b5e90e6
Iszdn <tu7k21xbe@code.edu.az>
2023-11-11 14:52:05 +0400

    cards for special price

--- client/assets/js.for.footer/customers.js
+++ client/assets/js.for.footer/customers.js
@@ -7,3 +7,31 @@ var swiper = new Swiper(".mySwiper", {
     },
    
   });
+
+  // special price js swiper
+  var swiper = new Swiper(".specialSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      }
+    },
+  });
