4708b1608905004c1e20524747acc8767848c057
Mehriban Khalilova <tu833KMN0@code.edu.az>
2023-11-12 01:25:00 +0400

    nura branch merged develop

--- client/assets/js/swiper.js
+++ client/assets/js/swiper.js
@@ -59,3 +59,41 @@ var swiper = new Swiper(".recomendedCard", {
       }
     },
 });
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
+ 
+});
+
+// special price js swiper
+var swiper = new Swiper(".specialSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    300: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    }
+  },
+});
